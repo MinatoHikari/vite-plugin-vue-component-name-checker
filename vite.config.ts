057868353e8e392fd7922d6f14510f9e50a9ca7b
MinatoHikari <35342316+MinatoHikari@users.noreply.github.com>
2023-07-27 18:08:28 +0800

    esmodule bundle

--- vite.config.ts
+++ vite.config.ts
@@ -7,12 +7,20 @@ import * as path from 'path';
 export default defineConfig({
     build: {
         lib: {
+            formats: ['umd'],
             entry: path.resolve(__dirname, './index.ts'),
             name: 'vite-plugin-vue-component-name-checker',
             fileName: 'index',
         },
-        rollupOptions: {},
-        minify: 'terser',
+        rollupOptions: {
+            external: ['node', 'camelcase', 'vite'],
+            output: {
+                // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+                globals: {
+                    node: 'Node',
+                },
+            },
+        },
         terserOptions: {
             compress: {
                 drop_console: true,
